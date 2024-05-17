bccc3241baddf67f49fe8bf031dddd68d55295d9
suhail-ak <suhailakramy12@gmail.com>
2024-05-18 00:52:23 +0200

    setup Modal

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="p-4 ">
-      <Button size="default" variant="destructive">
-        Click me
-      </Button>
-    </div>
-  );
-}
+import { useEffect } from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetpupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4 ">root</div>;
+};
+export default SetpupPage;
